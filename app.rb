3f094b772b6195fe2c7f55345b1df8cc3000c5f1
Joel Eche <joel.eche5@gmail.com>
2020-04-08 23:48:34 -0500

    Cache control

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require "sinatra"
 
 get "/:page?" do
   if params["page"].nil?
+    cache_control :public, max_age: 1
     erb :index, layout: :layout
   else
     erb params["page"].to_sym, layout: :layout
